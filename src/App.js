fef37c55478ce739e58a7475e4e81d9f5e46b614
‘athus.azevedo’ <athus.azevedo@bit.com.br>
2020-06-18 20:05:07 -0300

    Commit 01

--- src/App.js
+++ src/App.js
@@ -1,31 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+  
+  useEffect(()=>{
+    api.get('/repositories').then(response => {
+     setRepositories(response.data);
+   });
+ },[]);
+ 
   async function handleAddRepository() {
-    // TODO
+    
+    const response = await api.post('repositories',  { 
+      id: 111111,     
+      url: "https://github.com/josepholiveira",
+      title: "Desafio ReactJS",
+      techs: ["React", "Node.js"],
+    });
+
+    if(response.status == 200){
+      setRepositories([...repositories, response.data]);
+    }    
   }
 
-  async function handleRemoveRepository(id) {
-    // TODO
+  async function handleRemoveRepository(id) 
+  {
+    const response = await api.delete(`repositories/${id}`).then(()=>{
+      api.get('/repositories').then(response => {
+        
+        setRepositories(response.data);
+        console.log(response.data);       
+     });
+    });    
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+        {
+          repositories.map((repository) => {return( 
+            <li key={repository.id}>
+              {repository.title}
+          <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
           </button>
-        </li>
+            </li>)
+          })
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
+
+  
 }
 
 export default App;
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const { pathToFileURL } = require('url');
+const { dir } = require('console');
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                },
+
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use:[
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    }
+                ]
+            },
+            {
+                test: /.*\.(gif|png|jpe?g)$/i,                
+                use:[
+                    {
+                        loader: 'file-loader'
+                    }
+                ]
+            }
+        ]
+    }
+};
+
+/**
+ * $ yarn webpack-dev-server --mode development comando para Inicializar webPack
+ * 
+ */
